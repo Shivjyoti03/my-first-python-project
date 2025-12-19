536de4344eeaa435dad93f65dfbe2874417bd04e
shivjyoti <sheildvenom3@gmail.com>
2025-12-19 20:16:18 +0530

    Add files via upload

---
+++ myfirst.py
@@ -0,0 +1,24 @@
+print("hello world")
+print("my self shivjyoti kumar")
+print("i am from sitamarhi bihar")
+print("studing in iit patna")
+print("i love coding", "i love python programming language")
+age = 21
+height = 6.1
+
+
+print("my age is :", age)
+print("my height is :", height)
+
+fruits = ["mango", "banana", "orange"]
+
+
+
+
+
+
+
+
+
+
+
